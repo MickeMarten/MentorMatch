7f0b5c6134bc78bc013aa4bc0c1c58f39c8402ad
MickeMarten <KarlMikael.Martensson@gmail.com>
2024-01-12 16:26:36 +0100

    Added api with postreq

--- main.js
+++ main.js
@@ -11,6 +11,7 @@ class ApiFetch {
 
 
 class Render {
+   
     static async renderUsers() {
         const apiFetch = new ApiFetch();
         const users = await apiFetch.users();
@@ -45,38 +46,96 @@ class Render {
             cardContainer.classList.add('Section__cardContainer')
 
             });
+
+
         }
+        static CreateLogin(){
+       
+            const logInBtn = document.querySelector('.loginBtn');
+            
+            logInBtn.addEventListener('click', ()=> {
+            const bookModal = document.createElement('div');
+            document.body.append(bookModal);
+            bookModal.classList.add('booking__modal')
+
+            const fullNameInput = document.createElement('input')
+            fullNameInput.type='text'
+            fullNameInput.placeholder='För- och efternamn'
+            bookModal.appendChild(fullNameInput);
+            
+
+            const emailInput = document.createElement('input')
+            emailInput.type='text'
+            emailInput.placeholder='Email'
+            bookModal.append(emailInput);
+
+            const passWordInput = document.createElement('input')
+            passWordInput.placeholder='Lösenord';
+            passWordInput.type='text';
+            bookModal.append(passWordInput);
+
+            const sendBtn = document.createElement('button');
+            sendBtn.innerText='Skapa konto';
+            bookModal.append(sendBtn);
+            sendBtn.classList.add('sendBtn');
+
+            const sendUserInfo = new HandleUserInfo(sendBtn, emailInput, fullNameInput, passWordInput);
+            
+        })
     }
 
+    }
     Render.renderUsers();
+    Render.CreateLogin();
 
-    class LoggIn {
-       
-       static testar(){
-       
-                const logInBtn = document.querySelector('.nav__span');
-                
-                logInBtn.addEventListener('click', ()=> {
-                const bookModal = document.createElement('div');
-                document.body.append(bookModal);
-                bookModal.classList.add('booking__modal')
-               
-            })
+
+    
+    class HandleUserInfo{
+       constructor(sendBtn, emailInput, fullNameInput, passWordInput) {
+        this.emailInput = emailInput;
+        this.fullNameInput = fullNameInput;
+        this.passWordInput = passWordInput;
+        this.sendBtn = sendBtn;
+        sendBtn.addEventListener('click', () => this.createUser())
+       }
+
+       async createUser(){
+            const userEmailValue = this.emailInput.value;
+            const fullNameValue = this.fullNameInput.value;
+            const passwordValue = this.passWordInput.value;
+            console.log('Email:', userEmailValue);
+            console.log('Username:', fullNameValue);
+             console.log('Password:', passwordValue);
+
+             const userData = {
+                name: fullNameValue,
+                email: userEmailValue,
+                password: passwordValue
+
+             };
+
+                const response = await fetch ('http://localhost:3000/storeUser', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+
+                },
+                body:JSON.stringify(userData),
+
+             })
+
+            
         }
-        
-        
-      
-
-    }
-
-    LoggIn.testar();
-
-
-
+}
+    
+    
+   
     
 
 
 
-// // https://mocki.io/v1/d6cdabfa-c890-44c1-88cb-11ec56e3faf7
+
+
+    // // https://mocki.io/v1/d6cdabfa-c890-44c1-88cb-11ec56e3faf7
 
 
